d25322ef371a0f21a3a8f66eabe1996826385637
lisettemalacon <lmalacon123@gmail.com>
2022-05-26 16:21:53 -0700

    updates to choose_tracks

--- parse_saved_tracks.py
+++ parse_saved_tracks.py
@@ -3,21 +3,22 @@ import random
 
 auth.init()
 
+
 """
 Get the saved tracks for the current user and put all the saved track uri's into a list.
 Returns the list of saved track uri's. 
 """
-def get_saved_tracks():
+def get_saved_tracks(sp):
 	offset = 0
 	tracks = []
 
-	results = auth.sp.current_user_saved_tracks(limit=50)
+	results = sp.current_user_saved_tracks(limit=50)
 	while (results['items']):
 		for item in results['items']:
 			track = item['track']
 			tracks.append(track['uri'])
 		offset += 50
-		results = auth.sp.current_user_saved_tracks(limit=50, offset=offset)
+		results = sp.current_user_saved_tracks(limit=50, offset=offset)
 	return tracks
 
 
@@ -29,20 +30,44 @@ Returns a list of track uri's corresponding to user1's liked songs.
 '''
 def choose_tracks_user1(tracks):
 	likes = []
+	superlike = []
+	superliked = False
 	for i in range(20):
 		random_track = random.sample(tracks, 1)
 		track = auth.sp.track(random_track[0])
 		print(track['artists'][0]['name'], " - ", track['name'])
-		response = input("yes or no: ")
-		if (response.lower() == "yes" or "y"):
+		response = ''
+
+		if (not superliked):
+			response = input("yes, no, or SUPERLIKE? (y/n/s): " )
+		else:
+			response = input("yes or no? (y/n): " )
+
+		if (response.lower() == "yes" or response.lower() == "y"):
 			likes.append(random_track[0])
-	return likes
+		if ((response.lower() == 's' or response.lower() == 'SUPERLIKE') and not superliked):  # if they input superlike and didn't already superlike
+			superlike.append(random_track[0])
+			superliked = True
+		if ((response.lower() == 's' or response.lower() == 'SUPERLIKE') and superliked):
+			likes.append(random_track[0])
+	return likes, superlike
 
 
-def main():
-    tracks = get_saved_tracks()
-   	choose_tracks_user1(tracks)
-    
+def choose_tracks_user2(my_tracks, other_tracks):
+	matches = 0
+	total_votes = 0
+	matches_list = []
 
-if __name__ == '__main__':
-	 main()
+	while (matches < 5 and total_votes != len(my_tracks)):
+		random_track = random.sample(my_tracks, 1)
+		track = auth.sp.track(random_track[0])
+		print(track['artists'][0]['name'], " - ", track['name'])
+		response = input("yes or no: ")
+		if (response.lower() == "yes" or response.lower() == "y"):
+			if random_track[0] in other_tracks:
+				matches += 1
+				matches_list.append(random_track[0])
+		total_votes += 1
+
+	return matches_list
+
